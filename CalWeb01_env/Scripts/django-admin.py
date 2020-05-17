5664893893351e3fd06a93f3eb424a11b6b2ee63
Fidranth <pavk13@outlook.com>
2020-05-16 23:32:28 -0600

    CalWeb01_env files added

---
+++ CalWeb01_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\programming_master\projects\calweb01\calweb01_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
